689a56d384899d3bef08bdeb224aec1257852a48
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-03 14:38:46 +0100

    feat: solution for snack1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -2,12 +2,23 @@
 
 const userName = 'Mario';
 
-
 // Dichiara la funzione qui.
+// function
+function greeting(userNameFunc) {
+
+    return `Hi ${userNameFunc}`;
+}
+
+// arrow function
+const greetingArrow = (userNameArr) => `Hi ${userNameArr}`;
 
 
 // Invoca la funzione qui e stampa il risultato in console
+// print result with function
+console.log(greeting(userName));
 
+// print result with arrow function
+console.log(greetingArrow(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
