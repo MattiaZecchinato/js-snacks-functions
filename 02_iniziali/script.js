4722754da7db28937ee9e681a7bd3dee9c94e5d8
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-03 15:06:38 +0100

    feat: solution for snack2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,26 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+// function
+function firstLetterFunc(arrayNamesFunc) {
 
+    const arrayLetters = [];
+    let temp = '';
+
+    for (let i = 0; i < arrayNamesFunc.length; i++) {
+
+        temp = arrayNamesFunc[i];
+        arrayLetters.push(temp[0]);
+    }
+
+    return arrayLetters;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(firstLetterFunc(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
